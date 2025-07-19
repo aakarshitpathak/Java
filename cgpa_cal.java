98f3d9c57b6a4b8ada3c0c5286507ea8c9a0fc67
Aakarshit Pathak <139079683+aakarshitpathak@users.noreply.github.com>
2025-07-20 00:17:32 +0530

    Add files via upload

---
+++ cgpa_cal.java
@@ -0,0 +1,16 @@
+
+class cgpa_cal
+{
+    public static void main(String args[])
+    {
+        int a=45;
+        int b=95;
+        int c=48;
+        double avg=(a+b+c)/3;
+        double cgpa=avg/10;
+         System.out.println("average = "+ avg);
+         System.out.println("cgpa of your marks = "+ cgpa);
+        
+        
+    }
+}
