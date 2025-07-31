f84c56ba4b90d25ddb00466eca70ee406527aeed
Aakarshit Pathak <139079683+aakarshitpathak@users.noreply.github.com>
2025-08-01 00:04:36 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.lang.*;
+
+class pattern2
+{
+public static void main(String args[])
+{
+    for(int i=1; i<=5; i++)
+    {
+        for(int j=1; j<=5; j++)
+        {
+           System.out.print(i);
+        }
+    System.out.println();
+    }
+}
+}
